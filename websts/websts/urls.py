bea1d385a3e1e85af2203c796d16b36a442c5dc8
Julian Ridley <rankith117@hotmail.com>
2019-10-04 12:42:07 -0700

    MainApp

--- websts/websts/urls.py
+++ websts/websts/urls.py
@@ -19,8 +19,12 @@ Including another URLconf
 # Uncomment next two lines to enable admin:
 #from django.contrib import admin
 #from django.urls import path
+from django.conf.urls import include, url
+import main.views
 
 urlpatterns = [
+    url(r'^$', main.views.index, name='index'),
+    url(r'^home$', main.views.index, name='home'),
     # Uncomment the next line to enable the admin:
     #path('admin/', admin.site.urls)
 ]
